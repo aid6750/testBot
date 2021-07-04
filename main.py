d4d6b4206f91523eaf19610bb096c0ba50bbc282
Константин <konstantin.isakov.2003@mail.ru>
2021-07-04 21:20:08 +0700

    начало

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, executor
 from config import TOKEN
 loop = asyncio.get_event_loop()
-bot = Bot(TOKEN, parse_mode="HTML")
+bot = Bot(TOKEN)
 dp = Dispatcher(bot, loop=loop)
 
 
