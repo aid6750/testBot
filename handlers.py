5411e5fa143f0d548614d77eb69bfff092554345
Константин <konstantin.isakov.2003@mail.ru>
2021-07-05 11:52:47 +0700

    Добавлен повторный запуск теста

--- handlers.py
+++ handlers.py
@@ -4,66 +4,80 @@ from aiogram.dispatcher.filters import Command, Text
 from menu import menu, proposals
 from test import Test
 from random import randrange
+from aiogram.dispatcher import FSMContext
+from contextvars import ContextVar
+
+test = Test()
 
 
 @dp.message_handler(Command("test"))
 async def enter_test(message: Message):
-    praise = ("молодец", "так держать!", "здорово", "Ты просто монстр", "Продолжай в том же духе")
     await message.answer(text="Выберите испытание", reply_markup=menu)
 
-    @dp.message_handler(Text(equals=["Проверить навык сложения", "Проверить навык умножения"]))
-    async def on_button_menu_clicked(mode: Message):
-        if mode.text == "Проверить навык сложения":
-            mode = "+"
+
+@dp.message_handler(Text(equals=["Проверить навык сложения", "Проверить навык умножения"]))
+async def on_button_menu_clicked(message: Message):
+    global test
+    if message.text == "Проверить навык сложения":
+        test.mode = "+"
+    else:
+        test.mode = "*"
+
+    await message.answer(f'Сколько примеров вы хотите решить?', reply_markup=proposals)
+
+
+@dp.message_handler(Text(equals=["5", "10", "15", "20"]))
+async def on_button_proposals_clicked(message: Message):
+    global test
+    quantity = int(message.text)
+    await message.answer(f"желаю удачи, {message.from_user.username}")
+    test.quantity = quantity
+    await message.answer(f"ваш выбор:{test.quantity}")
+    operand1 = randrange(100)
+    operand2 = randrange(100)
+    question = f"сколько будет {operand1}{test.mode}{operand2}?"
+
+    if test.mode == "*":
+        test.rightAnswer = operand1 * operand2
+    else:
+        test.rightAnswer = operand1 + operand2
+
+    test.quantity = quantity
+    await message.answer(text=question, reply_markup=ReplyKeyboardRemove())
+    await test.question.set()
+
+
+@dp.message_handler(state=Test.question)
+async def answer(message: Message, state: FSMContext):
+    global test
+    praise = ("молодец", "так держать!", "здорово", "Ты просто монстр", "Продолжай в том же духе")
+    number = 0
+    try:
+        number = int(message.text)
+    except BaseException:
+        await message.answer("введите число")
+        return
+
+    test.counter += 1
+    if test.counter <= test.quantity:
+        if number == test.rightAnswer:
+            await message.answer(praise[randrange(len(praise))])
+            test.goodAnswers += 1
         else:
-            mode = "*"
-        await message.answer(f'Сколько примеров вы хотите решить?', reply_markup=proposals)
+            await message.answer("не верно")
 
-        @dp.message_handler(Text(equals=["5", "10", "15", "20"]))
-        async def on_button_proposals_clicked(quantity: Message):
-            quantity = int(quantity.text)
-            await message.answer(f"желаю удачи, {message.from_user.username}")
-            test = Test(quantity)
-            operand1 = randrange(100)
-            operand2 = randrange(100)
-            question = f"сколько будет {operand1}{mode}{operand2}?"
+        op1 = randrange(100)
+        op2 = randrange(100)
+        ques = f"сколько будет {op1}{test.mode}{op2}?"
 
-            if mode == "*":
-                test.rightAnswer = operand1 * operand2
-            else:
-                test.rightAnswer = operand1 + operand2
+        if test.mode == "*":
+            test.rightAnswer = op1 * op2
+        else:
+            test.rightAnswer = op1 + op2
 
-            await message.answer(text=question, reply_markup=ReplyKeyboardRemove())
-            await test.question.set()
-
-            @dp.message_handler(state=Test.question)
-            async def answer(ans: Message):
-                number = 0
-                try:
-                    number = int(ans.text)
-                except BaseException:
-                    await message.answer("введите число")
-                    await test.question.set()
-                    return
-
-                test.counter += 1
-                if number == test.rightAnswer:
-                    await message.answer(praise[randrange(len(praise))])
-                    test.goodAnswers += 1
-                else:
-                    await message.answer("не верно")
-
-                if test.counter < test.quantity:
-                    op1 = randrange(100)
-                    op2 = randrange(100)
-                    ques = f"сколько будет {op1}{mode}{op2}?"
-
-                    if mode == "*":
-                        test.rightAnswer = op1 * op2
-                    else:
-                        test.rightAnswer = op1 + op2
-
-                    await message.answer(text=ques)
-                    await test.question.set()
-                else:
-                    await message.answer(test.result())
+        await message.answer(text=ques)
+        await test.question.set()
+    else:
+        await message.answer(test.result())
+        await state.finish()
+        test.counter = 0
--- test.py
+++ test.py
@@ -8,14 +8,12 @@ class Test(StatesGroup):
     quantity = 0
     goodAnswers = 0
     rightAnswer = 0
-
-    def __init__(self, quantity):
-        self.quantity = quantity
+    mode = 0
 
     def result(self):
-        result = f"вы ответели правильно на {self.goodAnswers}"
+        result = f"Ты ответел правильно на {self.goodAnswers}"
 
-        if self.goodAnswers % 100 >= 10 and self.goodAnswers <= 20 or self.goodAnswers % 10 == 0:
+        if self.goodAnswers % 100 >= 10 and self.goodAnswers <= 20 or self.goodAnswers % 10 == 5:
             result += " вопросов "
         elif self.goodAnswers % 10 == 1:
             result += " вопрос "
@@ -25,7 +23,7 @@ class Test(StatesGroup):
         rating = ceil(self.goodAnswers / self.quantity * 5)
         if rating < 2:
             rating = 2
-        result += f"\nвы получили оценку {int(rating)}. "
+        result += f"\nи получил оценку {int(rating)}. "
         string_array = ("Не расстраивайся. Математика дана не всем",
                         "Не расстраивайся. Математика дана не всем", "Ты можешь лучше",
                         "Неплохой результат",
